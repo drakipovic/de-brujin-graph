3d1ddceb81b75baec27e40def16b95c3ed7cddba
tenap <tena.perak@photomath.net>
2017-01-13 10:17:18 +0100

    removed minor error from main

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,6 @@ int main(int argc, char **argv){
 
     std::string root_path = get_path();
 
-
     int d;
     root_path = root_path.substr(0, root_path.size()-8);
     std::string s = read(root_path + "/tests/" + argv[1], d);
@@ -42,12 +41,11 @@ int main(int argc, char **argv){
 
     char bwt[n + 1];
     std::deque<u_int> q;
-    std::vector<node> G(n);
+    std::vector<node> G;
     std::vector< std::pair<bool, bool>> bit_vectors;
     //std::vector< std::pair<bool, bool> > bit_vectors = create_bit_vectors(3, bwt, d, s, G, q);
     //for(int i = 1; i < n+1; ++i) std::cout << bit_vectors[i].second << " " << bit_vectors[i].first << std::endl;
 
- 
     G = create_implicit_graph(k, bwt, s, d, bit_vectors);
     // for(int i = 1; i < n+1; ++i) std::cout << bit_vectors[i].second << " " << bit_vectors[i].first << std::endl;
     
